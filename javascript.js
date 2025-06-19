61bd1428aa80001a097a685e53f9f8a5d599d87a
Denigirl02 <58830475+Denigirl02@users.noreply.github.com>
2025-06-19 07:28:05 +0200

    Link elements to js script

--- javascript.js
+++ javascript.js
@@ -1,7 +1,38 @@
-console.log(numberAdd(5, 5));
-console.log(numberSubtract(6, 2));
-console.log(numberMultiply(5, 5));
-console.log(numberDivide(10, 2));
+let firstNumber;
+let secondNumber;
+let operator;
+let screenTop = "";
+let screenBottom = "";
+let screenOperator = "";
+
+// Buttons
+const btn1 = document.getElementById("btn_1");
+const btn2 = document.getElementById("btn_2");
+const btn3 = document.getElementById("btn_3");
+const btn4 = document.getElementById("btn_4");
+const btn5 = document.getElementById("btn_5");
+const btn6 = document.getElementById("btn_6");
+const btn7 = document.getElementById("btn_7");
+const btn8 = document.getElementById("btn_8");
+const btn9 = document.getElementById("btn_9");
+const btn0 = document.getElementById("btn_0");
+const btnPoint = document.getElementById("btn_point");
+const btnAdd = document.getElementById("btn_add");
+const btnSubtract = document.getElementById("btn_subtract");
+const btnMultiply = document.getElementById("btn_multiply");
+const btnDivide = document.getElementById("btn_divide");
+const btnEquals = document.getElementById("btn_equals");
+const btnDelete = document.getElementById("btn_delete");
+const btnAC = document.getElementById("btn_ac");
+
+// Others
+const textTop = document.getElementById("topText");
+const textBottom = document.getElementById("rightSide");
+const textOperator = document.getElementById("leftSide");
+
+btn_1.addEventListener("click", function () {
+	populateBottomText(1);
+});
 
 // FUNCTIONS
 function numberAdd(a, b) {
@@ -22,4 +53,30 @@ function numberMultiply(a, b) {
 function numberDivide(a, b) {
 	const result = a / b;
 	return result;
+}
+
+function operate(firstNumber, secondNumber, operator) {
+	let result;
+	switch (operator) {
+		case "add":
+			result = numberAdd(firstNumber, secondNumber);
+			break;
+		case "subtract":
+			result = numberSubtract(firstNumber, secondNumber);
+			break;
+		case "multiply":
+			result = numberMultiply(firstNumber, secondNumber);
+			break;
+		case "divide":
+			result = numberDivide(firstNumber, secondNumber);
+		default:
+			break;
+	}
+	return result;
+}
+function clearBottomText() {
+	textBottom.textContent = "";
+}
+function populateBottomText(number) {
+	textBottom.textContent = textBottom.textContent + number;
 }
