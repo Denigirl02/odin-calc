132a8f6206effbb01e32f282022c05a3801581d9
Denigirl02 <58830475+Denigirl02@users.noreply.github.com>
2025-06-19 07:00:30 +0200

    Add basic math functions

---
+++ javascript.js
@@ -0,0 +1,25 @@
+console.log(numberAdd(5, 5));
+console.log(numberSubtract(6, 2));
+console.log(numberMultiply(5, 5));
+console.log(numberDivide(10, 2));
+
+// FUNCTIONS
+function numberAdd(a, b) {
+	const result = a + b;
+	return result;
+}
+
+function numberSubtract(a, b) {
+	const result = a - b;
+	return result;
+}
+
+function numberMultiply(a, b) {
+	const result = a * b;
+	return result;
+}
+
+function numberDivide(a, b) {
+	const result = a / b;
+	return result;
+}
